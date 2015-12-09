56152a30423874ef88dc26b5836e772c69be7433
Ruben Luna <rubenlunamate@gmail.com>
2015-12-08 22:30:25 -0430

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,16 +6,6 @@
 #include "DoblementeEnlazadaCircular.h"
 #include "ConSaltosOrdenada.h"
 
-void mostrarD(doble *p) /*Muestra la lista principal completa.*/
-{
-	doble *aux = p;
-	while (aux)
-	{
-		printf(" %i ->", aux->valor);
-		aux = aux->prox;
-	}
-	printf(" NULL");
-}
 
 void mostrarC(listaC *p) /*Muestra la lista principal completa.*/
 {
@@ -31,24 +21,52 @@ void mostrarC(listaC *p) /*Muestra la lista principal completa.*/
 	printf(" NULL");
 }
 
+void mostrarD(doble *p) /*Muestra la lista principal completa.*/
+{
+	doble *aux = p;
+	while (aux)
+	{
+		printf(" %i ->", aux->valor);
+		aux = aux->prox;
+	}
+	printf(" NULL");
+}
+
+void mostrarDC(dobleC *r) /*Muestra la lista principal completa.*/
+{
+	if (!vacioDC(r))
+	{
+		printf(" %i ->", r->valor);
+		dobleC *aux = r;
+		while (aux->prox->valor != r->valor)
+		{
+			printf(" %i ->", aux->prox->valor);
+			aux = aux->prox;
+		}
+	}
+	printf(" NULL");
+}
+
+
 void main()
 {
 	int op = -1, n = 0;
-	doble *p = NULL;
 	listaC *q = NULL;
+	doble *p = NULL;
+	dobleC *r = NULL;
 
 	while (op != 0) {
 		system("cls");
 		printf("\n\n                              Menu principal! \n\n\n");
 		printf("\n 1. Insertar en lista circular .");
 		printf("\n 2. Insertar en la lista doblemente enlazada."); 
-		printf("\n 3. Mostrar lista circular.");
-		printf("\n 4. Mostrar lista doblemente enlazada.");
-	//	printf("\n 5. Mostrar todo."); 
-		printf("\n 6. Eliminar un elemento (x) de la lista circular.");
-		printf("\n 7. Eliminar un elemento (x) de la lista doblemente enlazada.");
-	/*	printf("\n 8. Guardar datos en un archivo."); 
-		printf("\n 9. Leer datos desde el archivo."); */
+		printf("\n 3. Insertar en la lista doblemente enlazada circular.");
+		printf("\n 4. Mostrar lista circular.");
+		printf("\n 5. Mostrar lista doblemente enlazada."); 
+		printf("\n 6. Mostrar lista doblemente enlazada circular.");
+		printf("\n 7. Eliminar un elemento (x) de la lista circular.");
+		printf("\n 8. Eliminar un elemento (x) de la lista doblemente enlazada."); 
+		printf("\n 9. Eliminar un elemento (x) de la lista doblemente enlazada circular."); 
 		printf("\n\n 0. Salir. \n\n");
 		printf(" Indique opcion : ");
 		op = 0;
@@ -62,41 +80,43 @@ void main()
 			insertarC(&q, n);
 			break;
 
-		 case 2:
-			printf("\n\n Numero a insertar en la lista :");
+		 case 2: printf("\n\n Numero a insertar en la lista :");
 			scanf("%i", &n);
 			insertarD(&p, n);
 			break;
 
-		case 3:
-			printf("\n\n Lista Circular \n\n ");
+		 case 3: printf("\n\n Numero a insertar en la lista :");
+			 scanf("%i", &n);
+			 insertarDC(&r, n);
+			 break;
+
+		case 4: printf("\n\n Lista Circular \n\n ");
 			mostrarC(q);
 			break;
-		case 4:
-			printf("\n\n Lista Doblemente Enlazada \n\n ");
+
+		case 5: printf("\n\n Lista Doblemente Enlazada \n\n ");
 			mostrarD(p);
 			break;
-		/*
-		case 4: printf("\n\n Sub-lista. \n\n");
-			printf("Numero a buscar para imprimir la sublista :");
-			scanf("%i", &n);
-			mostrar2(p, n);
+
+		case 6: printf("\n\n Lista Doblemente Enlazada Circular \n\n ");
+			mostrarDC(r);
 			break;
 
-		case 5: printf("\n\n                      Lista principal con sus sub-listas: \n\n");
-			mostrarTodo(p);
-			break;
-*/
-		case 6:
+		case 7:
 			printf("\n\n Numero a buscar para eliminar de la lista :");
 			scanf("%i", &n);
 			eliminarC(&q, n);
 			break;
 
-		case 7: printf("\n\n Numero a buscar para eliminar de la lista :");
+		case 8: printf("\n\n Numero a buscar para eliminar de la lista :");
 			scanf("%i", &n);
 			eliminarD(&p, n);
 			break; 
+
+		case 9: printf("\n\n Numero a buscar para eliminar de la lista :");
+			scanf("%i", &n);
+			eliminarDC(&r, n);
+			break;
 		}
 		if (op)
 		{
